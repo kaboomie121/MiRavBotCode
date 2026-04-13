ba8fdb6be1ef7eb8e3a729159816713e6782dc5b
Jorn De Wolf (Laptop) <jorn.de.wolf@student.howest.be>
2026-04-13 23:21:42 +0200

    Setup SQB_Battle_Rating

--- helperFunctions/SQB_battle_rating.py
+++ helperFunctions/SQB_battle_rating.py
@@ -3,6 +3,11 @@ from pathlib import Path
 logging.getLogger(__name__)
 logging.info(f'Importing {Path(__file__).name}')
 
-import discord
 from config_loader import config
+import discord
+import requests
 
+def GetCurrentSquadronSchedule():
+    result = requests.get("https://forum.warthunder.com/t/season-schedule-for-squadron-battles/4446")
+    if result.status_code == 200:
+        logging.info()
